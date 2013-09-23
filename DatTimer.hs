6849cd5b2ac1c676a68f1ec0b9588864c5f31eb3
Art <cipherwraith666@gmail.com>
2013-09-23 23:19:22 +0800

    Optimizations
    Should see much better performance now

--- DatTimer.hs
+++ DatTimer.hs
@@ -5,17 +5,11 @@ import qualified Data.Map.Lazy as M
 import Control.Applicative
 import System.Directory
 import Data.Maybe
-
-serverDirectory = "/home/serve/script/"
-
-secondsFile   :: String
-secondsFile   = serverDirectory ++ "seconds.dat"      -- Delete this dat file once every second
-
-minuteFile    :: String
-minuteFile    = serverDirectory ++ "minutes.dat"      -- Delete this dat file once every minute
-
-hourFile      :: String
-hourFile      = serverDirectory ++ "hours.dat"        -- Delete this dat file once every hour
+import Data.Monoid
+import qualified Data.ByteString.Lazy as BL hiding (readFile, writeFile, pack)
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Control.Concurrent
+import System.Posix.Files
 
 maxPerSecond :: Int
 maxPerSecond = 10
@@ -26,72 +20,82 @@ maxPerMinute = 60
 maxPerHour :: Int
 maxPerHour = 600 
 
--- if user is over their dat limit, then this will return True
---isUserOverDatLimit :: String -> IO Bool
+
+serverDirectory = "/home/serve/script/"
+datsDirectory = serverDirectory ++ "dats/"
+minutesDirectory = "/home/serve/script/dats/hours/"
+hoursDirectory = "/home/serve/script/dats/minutes/"
+secondsDirectory = "/home/serve/script/dats/seconds/"
+directoriesToCheck = [serverDirectory, datsDirectory, minutesDirectory, hoursDirectory, secondsDirectory]
+
+secondsFile   :: String -> String
+secondsFile x = mconcat [datsDirectory, "seconds/", x, ".dat"]      -- Delete this dat file once every second
+
+minuteFile    :: String -> String
+minuteFile  x = mconcat [datsDirectory, "minutes/", x, ".dat"]      -- Delete this dat file once every minute
+
+hourFile      :: String -> String
+hourFile    x = mconcat [datsDirectory, "hours/", x, ".dat"]        -- Delete this dat file once every hour
+
+
 isUserOverDatLimit :: String -> IO Bool
-isUserOverDatLimit userHash = do
-  sList <- secondsListCount -- Amount of times user accessed in last second
-  if fromMaybe 1 sList > maxPerSecond -- check if user is over max second limit
-    then return True -- if over then return true
-    else do
-      mList <- minutesListCount
-      if fromMaybe 1 mList > maxPerMinute  -- if not over, then check the minute limit
-        then return True
-        else do
-          hList <- hourListCount 
-          return $ fromMaybe 1 hList > maxPerHour -- if not over, then check the hour limit
+isUserOverDatLimit userHash = checkFilesForLimit fileList
   where
-    secondsListCount :: IO (Maybe Int)
-    secondsListCount = M.lookup userName <$> secondsList
+    primeHash = removePunctuation userHash
+    fileList = [(secondsFile primeHash, maxPerSecond), (minuteFile primeHash, maxPerMinute), (hourFile primeHash, maxPerHour)]
 
-    minutesListCount :: IO (Maybe Int)
-    minutesListCount = M.lookup userName <$> minutesList
+-- Returns True if the user is over their speedlimit, false if they are not
+checkFilesForLimit :: [(FilePath, Int)] -> IO Bool
+checkFilesForLimit [] = return False
+checkFilesForLimit (x:xs) = do
+  overLimit <- checkFileForLimit x
+  if overLimit 
+    then return True
+    else checkFilesForLimit xs
 
-    hourListCount :: IO (Maybe Int)
-    hourListCount = M.lookup userName <$> hourList
+-- Returns True if the user is over their speedlimit, false if they are not
+checkFileForLimit :: (FilePath, Int) -> IO Bool
+checkFileForLimit (fileName, speedLimit) = do
+  existence <- doesFileExist fileName
+  if existence
+    then do
+      fileIn <- BL.readFile fileName
+      return . checkSpeed . modify $ fileIn
+    else return False
+  where
+    modify = fst . fromJust . BL.readInt
+    checkSpeed = (<) speedLimit
 
-    userName :: T.Text
-    userName = T.pack userHash
+removePunctuation :: String -> String
+removePunctuation x = doWork x ""
+  where
+    doWork [] y = y
+    doWork (x:xs) y
+      | x == '.' = doWork xs y
+      | x == '/' = doWork xs y
+      | otherwise = doWork xs (x:y)
+
 
 addUserToTimer :: String -> IO ()
 addUserToTimer userHash = do
-  T.appendFile secondsFile output
-  T.appendFile minuteFile output
-  T.appendFile hourFile output
+  mapM_ (forkIO . addToTimer) fileList
   where
     output = T.pack $ userHash ++ "\n"
+    primeHash = removePunctuation userHash
+    fileList = [secondsFile primeHash, minuteFile primeHash, hourFile primeHash]
 
-secondsList :: IO (M.Map T.Text Int)
-secondsList = do
-  existence <- doesFileExist secondsFile
-  if existence
+addToTimer :: FilePath -> IO ()
+addToTimer fileName = do
+  existence <- doesFileExist fileName
+  if existence 
     then do
-      fileIn <- T.readFile secondsFile
-      let modFile = M.fromListWith (+) $ zip (T.lines fileIn) [1,1..]
-      return modFile
-    else return basicMap
-
-minutesList :: IO (M.Map T.Text Int)
-minutesList = do
-  existence <- doesFileExist minuteFile
-  if existence
-    then do
-      fileIn <- T.readFile minuteFile
-      let modFile = M.fromListWith (+) $ zip (T.lines fileIn) [1,1..]
-      return modFile
-    else return basicMap
-
-hourList :: IO (M.Map T.Text Int)
-hourList = do
-  existence <- doesFileExist hourFile
-  if existence
-    then do
-      fileIn <- T.readFile hourFile
-      let modFile = M.fromListWith (+) $ zip (T.lines fileIn) [1,1..]
-      return modFile
-    else return basicMap
-
-basicMap :: M.Map T.Text Int
-basicMap = M.fromList [(T.pack "", 0)]
+      fileContents <- BL.readFile fileName
+      let a = BL.length fileContents
+      seq a $ BL.writeFile fileName $ modified fileContents
+    else do
+      BL.writeFile fileName . BL.pack $ "0"
+ where
+  modified = BL.pack . show . (+ 1) . fst . fromJust . BL.readInt 
+  
 
 
