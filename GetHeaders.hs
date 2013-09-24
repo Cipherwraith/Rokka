49682bd2bbbdebbf94d7d35d47aed04f1692f0e5
Art <cipherwraith666@gmail.com>
2013-09-25 00:03:18 +0800

    Added some comments

--- GetHeaders.hs
+++ GetHeaders.hs
@@ -6,13 +6,17 @@ import Data.Maybe
 import qualified Control.Exception as X
 import Network.Stream
 
+-- Downloads the URL and isolates the response headers
 download url = liftA rspHeaders <$> runHTTP url
 
+-- Do a head request, and catch any exceptions.
 runHTTP url = (simpleHTTP (headRequest url) ) `X.catch` exceptionHandler
 
+-- IF there is any exception in the HTTP head request, then replace it with a request to awaq.com
 exceptionHandler :: X.SomeException -> IO (Result (Response String))
 exceptionHandler x = simpleHTTP (headRequest "http://www.awaq.com/")
 
+-- Pattern match on the Either functor
 checkRightOrLeft (Left x) = []
 checkRightOrLeft (Right x) = x
 
@@ -21,6 +25,8 @@ checkRightOrLeft (Right x) = x
 contentType' = HdrContentType
 lastModified' = HdrLastModified
 
+-- Gets headers and isolates a single header type. See contentType' and lasModified' for an example of header type
 getHeader x url = fromMaybe "" . lookupHeader x . checkRightOrLeft <$> download url
 
+-- Sugar to get the "lastmodified" header and isolate it out into a string
 getLastModified = getHeader lastModified'
